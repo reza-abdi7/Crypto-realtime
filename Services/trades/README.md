90527f9cbdf6d8863cd9727052992cfdc62b695b
Reza Abdi <reza.abdi@live.com>
2024-12-09 09:56:07 +0100

    pushing working trades signals from kraken to kafka topic, but not fully dockerized yet.

--- Services/trades/README.md
+++ Services/trades/README.md
@@ -1,3 +1,5 @@
 ## Trade Service
 
-it ingest trades from the kraken API and push them to kafka
+it ingest trades from the kraken API and push them to kafka topic.
+
+
--- Services/trades/run.py
+++ Services/trades/run.py
@@ -1,6 +1,13 @@
-import loguru as logger
+from loguru import logger
+from quixstreams import Application
 
-def main():
+from kraken_api.websocket import KrakenWebsocketAPI
+
+def main(
+    kafka_broker_address: str,
+    kafka_topic: str,
+    kraken_api: KrakenWebsocketAPI
+):
     """
     reads trades from the kraken API and push them to kafka topics.
     
@@ -9,14 +16,51 @@ def main():
     2. push the trades to kafka topic.
     
     Args:
-        None
-
+        kafka_broker_address (str): the address of the kafka broker
+        kafka_topic (str): the topic to push the trades to
+        kraken_api (KrakenWebsocketAPI): the kraken api to read trades from
     Returns:
         None
     """
     logger.info("Starting trades service")
+
+    # initialize the quixstreams application
+    # this class will handle the low-level details to connect to kafka.
+    app = Application(
+        broker_address=kafka_broker_address
+    )
     
+    # Define a topic where the trades will be pushed
+    topic = app.topic(name=kafka_topic, value_serializer='json')
+
+    # Create a Producer instance
+    with app.get_producer() as producer:
+
+        while True:
+            trades = kraken_api.get_trades()
+
+            for trade in trades:
+                # serialize the trade to json
+                message = topic.serialize(
+                    key=trade.pair,
+                    value=trade.to_dict()
+                )
+
+                # push the serialized message to the topic
+                producer.produce(topic=topic.name, value=message.value, key=message.key)
+
+                logger.info(f"Pushed trade {trade} to topic {kafka_topic}")
 
 
 if __name__ == "__main__":
-    main()
+
+    from config import config
+
+    # initialize the kraken api
+    kraken_api = KrakenWebsocketAPI(config.pairs)
+
+    main(
+        kafka_broker_address=config.kafka_broker_address,
+        kafka_topic=config.kafka_topic,
+        kraken_api=kraken_api
+        )
