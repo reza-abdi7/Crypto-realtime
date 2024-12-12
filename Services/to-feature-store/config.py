92f550286b3ab7f0e356845cac8896a817fc67e6
Reza Abdi <reza.abdi@live.com>
2024-12-12 13:07:33 +0100

    feature store up and running, initial test done, not dockerized

--- Services/to-feature-store/config.py
+++ Services/to-feature-store/config.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -10,11 +12,14 @@ class Settings(BaseSettings):
     kafka_consumer_group: str
     feature_group_name: str
     feature_group_version: int
+    feature_group_primary_key: List[str]
+    feature_group_event_time: str
 
 
 class HopsworksCredentials(BaseSettings):
     model_config = SettingsConfigDict(env_file='credentials.env')
     hopsworks_api_key: str
+    hopsworks_project_name: str
 
 
 config = Settings()
--- Services/to-feature-store/run.py
+++ Services/to-feature-store/run.py
@@ -1,13 +1,13 @@
 from loguru import logger
 from quixstreams import Application
+from sink import HopsworksFeatureStoreSink
 
 
 def main(
     kafka_broker_address: str,
     kafka_input_topic: str,
     kafka_consumer_group: str,
-    feature_group_name: str,
-    feature_group_version: int,
+    output_sink: HopsworksFeatureStoreSink,
 ):
     """
     2 things:
@@ -29,28 +29,38 @@ def main(
         consumer_group=kafka_consumer_group,
     )
 
-    # input_topic = app.topic(
-    #     name=kafka_input_topic,
-    #     value_deserializer='json',
-    # )
+    input_topic = app.topic(
+        name=kafka_input_topic,
+        value_deserializer='json',
+    )
+
+    sdf = app.dataframe(input_topic)
 
     # Do some processing here ...
     # We need to extract the features we want to push to the feature store
     # TODO: Implement
     # Sink data to a CSV file
     # sdf.sink(csv_sink)
+    sdf.sink(output_sink)
 
     app.run()
     # push messages to the feature store
 
 
 if __name__ == '__main__':
-    from config import config
+    from config import config, hopsworks_credentials
 
+    hopsworks_sink = HopsworksFeatureStoreSink(
+        api_key=hopsworks_credentials.hopsworks_api_key,
+        project_name=hopsworks_credentials.hopsworks_project_name,
+        feature_group_name=config.feature_group_name,
+        feature_group_version=config.feature_group_version,
+        feature_group_primary_key=config.feature_group_primary_key,
+        feature_group_event_time=config.feature_group_event_time,
+    )
     main(
         kafka_broker_address=config.kafka_broker_address,
         kafka_input_topic=config.kafka_input_topic,
         kafka_consumer_group=config.kafka_consumer_group,
-        feature_group_name=config.feature_group_name,
-        feature_group_version=config.feature_group_version,
+        output_sink=hopsworks_sink,
     )
